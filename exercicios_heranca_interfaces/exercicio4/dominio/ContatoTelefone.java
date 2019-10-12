f900632c7ab151703dbe443dd309c9c3f94e348b
Matheus Figueiredo <matheus_figueiredo18@hotmail.com>
2019-10-12 18:19:32 -0300

    Criação da classe ContatoTelefone.

---
+++ exercicios_heranca_interfaces/exercicio4/dominio/ContatoTelefone.java
@@ -0,0 +1,20 @@
+package com.paradigmas.lab.exercicios_heranca_interfaces.exercicio4.dominio;
+
+public class ContatoTelefone implements Contato{
+
+	@Override
+	public String getNome() {
+		return null;
+	}
+
+	@Override
+	public String getContato() {
+		return null;
+	}
+
+	@Override
+	public String getTipo() {
+		return null;
+	}
+	
+}
