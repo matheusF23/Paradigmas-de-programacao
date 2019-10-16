c51fbcde10723f1e10693b9f50480d058b7adc04
Matheus Figueiredo <matheus_figueiredo18@hotmail.com>
2019-10-16 09:50:09 -0300

    Criacao do pacote interfaces.

---
+++ interfaces/OlaSwing.java
@@ -0,0 +1,14 @@
+package com.paradigmas.lab.interfaces;
+
+import javax.swing.JFrame;
+
+public class OlaSwing {
+
+	public static void main(String[] args) {
+		JFrame frame = new JFrame();
+		frame.setVisible(true);
+		frame.setSize(200, 200);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
