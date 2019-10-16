7d8e0a1136af11271c35851fa15616bdd2278d64
Matheus Figueiredo <matheus_figueiredo18@hotmail.com>
2019-10-16 18:15:51 -0300

    Adicao de evento para acao do botao. Organizacao do codigo.

--- calculaimc/CalculadoraIMC.java
+++ calculaimc/CalculadoraIMC.java
@@ -1,8 +1,5 @@
 package com.paradigmas.lab.calculaimc;
 
-
-
-/*
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -11,10 +8,10 @@ import java.text.DecimalFormat;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 
-class InterfaceUsuario extends JFrame{
+class Interface extends JFrame{
+	
 	private JLabel lblPeso;
 	private JTextField txtPeso;
 	private JLabel lblAltura;
@@ -22,19 +19,15 @@ class InterfaceUsuario extends JFrame{
 	private JLabel lblResultado;
 	private JButton btnCalcula;
 	
-	public InterfaceUsuario() {
-		setConfig(); // Cada vez que chamar o construtor ja será feita a configuração
-	}
-
-	private void setConfig() {
+	public Interface(){
 		// Configurando o Jframe
 		this.setTitle("Calculador de IMC"); // Titulo
-		this.setSize(600, 100); //Tamanho
+		this.setSize(500, 100); //Tamanho
 		this.setLayout(new GridLayout(0,2));
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE); // Fecha a aplicacao
 		this.setLocationRelativeTo(null); // Centraliza o Jframe
 		
-		
+		// Instanciando
 		lblPeso = new JLabel("Peso: ");
 		txtPeso = new JTextField();
 		lblAltura = new JLabel("Altura: ");
@@ -43,7 +36,27 @@ class InterfaceUsuario extends JFrame{
 		btnCalcula = new JButton("Calcular");
 		
 		// Evento para calcular o IMC
-		btnCalcula.addActionListener(new CalculoIMC());
+		btnCalcula.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				double peso = Double.parseDouble(txtPeso.getText());
+				double altura = Double.parseDouble(txtAltura.getText());
+				double imc = peso / Math.pow(altura, 2);
+				
+				DecimalFormat df = new DecimalFormat("#0.0");
+				String resultado = "IMC: " + df.format(imc);
+				
+				if(imc < 18.5){resultado+=". Abaixo do peso";}
+                else if(imc < 24.9){resultado+=". Peso ideal";}
+                else if(imc < 29.9){resultado+=". Levemente acima do peso";}
+                else if(imc < 34.9){resultado+=". Primeiro grau de obesidade";}
+                else if(imc < 39.9){resultado+=". Segundo grau de obesidade";}
+                else resultado+=" Obesidade mórbida";
+				
+				lblResultado.setText(resultado);
+			}
+			
+		});
 		
 		// Adicionando componentes ao JFrame
 		this.add(lblPeso);
@@ -51,60 +64,14 @@ class InterfaceUsuario extends JFrame{
 		this.add(lblAltura);
 		this.add(txtAltura);
 		this.add(lblResultado);
-		this.add(btnCalcula);
-		
+		this.add(btnCalcula);	
 	}
-	
-	// classe interna para o botao do calculo
-	class CalculoIMC implements ActionListener {
-
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			try {
-				double peso = Double.parseDouble(txtPeso.getText());
-				double altura = Double.parseDouble(txtAltura.getText());
-				double imc = peso / Math.pow(altura, 2);
-				
-				 DecimalFormat df = new DecimalFormat("#0.0");
-				
-				String resultado = "IMC: " + df.format(imc);
-				
-				if(imc < 18.5){
-                    resultado+=" Abaixo do peso";
-                }
-                else if(imc < 24.9){
-                    resultado+=" Peso ideal";
-                }
-                else if(imc < 29.9){
-                    resultado+=" Levemente acima do peso";
-                }
-                else if(imc < 34.9){
-                    resultado+=" Primeiro grau de obesidade";
-                }
-                else if(imc < 39.9){
-                    resultado+=" Segundo grau de obesidade";
-                }
-                else
-                    resultado+=" Obesidade mórbida";
-				
-				lblResultado.setText(resultado);
-			}catch (ArithmeticException ar) {
-                JOptionPane.showMessageDialog(null, "Erro aritmético, causa: "+ar.getMessage());
-            } catch (Exception ex) {
-                JOptionPane.showMessageDialog(null, "Erro desconhecido, causa: "+ex.getMessage());
-            }
-		}
-
-	}
-
 }
-*/
+
 
 public class CalculadoraIMC {
 	public static void main(String[] args) {
 		Interface frame = new Interface();
-		
 		frame.setVisible(true);
-		
 	}
 }
