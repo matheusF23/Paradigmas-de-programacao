d386709e145d6f571a720f1d1553b1ae8b7d36ab
Matheus Figueiredo <matheus_figueiredo18@hotmail.com>
2019-10-18 11:03:05 -0300

    Adicao do metodo calculaIMC.

--- calculaimc/CalculadoraIMC.java
+++ calculaimc/CalculadoraIMC.java
@@ -1,8 +1,6 @@
 package com.paradigmas.lab.calculaimc;
 
 import java.awt.FlowLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.text.DecimalFormat;
 
 import javax.swing.JButton;
@@ -55,33 +53,15 @@ class FrameCalculator extends JFrame{
 		jpnButton = new JPanel();
 		
 		// Evento para calcular o IMC
-		btnCalcula.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				double peso = Double.parseDouble(txtPeso.getText());
-				double altura = Double.parseDouble(txtAltura.getText());
-				double imc = peso / Math.pow(altura, 2);
-				
-				DecimalFormat df = new DecimalFormat("#0.0");
-				String resultado = "IMC: " + df.format(imc);
-				
-				if(imc < 18.5){resultado+=". Abaixo do peso";}
-                else if(imc < 24.9){resultado+=". Peso ideal";}
-                else if(imc < 29.9){resultado+=". Levemente acima do peso";}
-                else if(imc < 34.9){resultado+=". Primeiro grau de obesidade";}
-                else if(imc < 39.9){resultado+=". Segundo grau de obesidade";}
-                else resultado+=" Obesidade mórbida";
-				
-				lblResultado.setText(resultado);
-			}
-			
+		btnCalcula.addActionListener((ev) -> {
+			calculaIMC();
 		});
 		
 		// Evento para limpar os campos
 		btnLimpar.addActionListener((ev) -> {
-				txtPeso.setText("");
-				txtAltura.setText("");
-				lblResultado.setText("IMC: ");
+			txtPeso.setText("");
+			txtAltura.setText("");
+			lblResultado.setText("IMC: ");
 		});
 		
 		// Evento para fechar aplicacao
@@ -103,6 +83,24 @@ class FrameCalculator extends JFrame{
 		add(jpnAltura);
 		add(jpnButton);
 		add(lblResultado);
+	}
+	
+	public void calculaIMC() {
+		double peso = Double.parseDouble(txtPeso.getText());
+		double altura = Double.parseDouble(txtAltura.getText());
+		double imc = peso / Math.pow(altura, 2);
+		
+		DecimalFormat df = new DecimalFormat("#0.0");
+		String resultado = "IMC: " + df.format(imc);
+		
+		if(imc < 18.5){resultado += ". Abaixo do peso";}
+        else if(imc < 24.9){resultado += ". Peso ideal";}
+        else if(imc < 29.9){resultado += ". Levemente acima do peso";}
+        else if(imc < 34.9){resultado += ". Primeiro grau de obesidade";}
+        else if(imc < 39.9){resultado += ". Segundo grau de obesidade";}
+        else resultado += " Obesidade mórbida";
+		
+		lblResultado.setText(resultado);
 	}
 }
 
